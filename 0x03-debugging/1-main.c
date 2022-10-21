22776857e392a4ac0836972eb96e888c01bb53fe
yunishello <yunishello@gmail.com>
2022-10-20 23:29:49 -0700

    added

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,14 +7,15 @@
 
 int main(void)
 {
-	int i;
-
-	printf("Infinite loop comming :(\n");
-	i = 0;
-	while (i < 10)
-	{
-		/* putchar(i); */
-	}
+	/*
+	 * int i;
+	 * printf("Infinite loop comming :(\n");
+	 * i = 0;
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
