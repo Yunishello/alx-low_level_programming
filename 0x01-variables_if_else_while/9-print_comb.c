19f3b769477a168aca4f36b99ad1cb0d7835a019
yunishello <yunishello@gmail.com>
2022-10-24 19:22:47 -0700

    pages added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print all posible outcome of a singlt-digit number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
