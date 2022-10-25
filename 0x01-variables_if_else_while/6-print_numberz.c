8472065962febe0cd813b0b78b3a67f76516e749
yunishello <yunishello@gmail.com>
2022-10-24 18:22:42 -0700

    main changes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all digit number 0f base 10 starting from 0
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
