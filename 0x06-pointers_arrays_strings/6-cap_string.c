3066237cd092eba2b79f5f56b89d37db86e60577
yunishello <yunishello@gmail.com>
2022-10-30 20:03:11 -0700

    6-cap_string.c new changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -31,8 +31,9 @@ char *cap_string(char *s)
 int check_seperators(char c)
 {
 	int i = 0;
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?','"',
-		'(', ')', '{', '}' };
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+
 	for (; i < 13; i++)
 		if (c == seperators[i])
 			return (1);
