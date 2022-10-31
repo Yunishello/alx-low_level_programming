92f5391980e73a8657e8fb3aa915776c9491b38a
yunishello <yunishello@gmail.com>
2022-10-30 18:09:58 -0700

    0-reset_to_98.c added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - Function that take int as parameter and update it pointer that it point to to 98
+ *
+ * Return: 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that  print n element of an array of integer and print new line
+ *@print_array: accept parametal a and n
+ *@a: accept an integer
+ *@n: accept an integer
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
+	}
+	printf("\n");
+}
